180ca6a718d0e664c389257c865e74b8aac26dd4
shinkunworld <47812055+shinkunworld@users.noreply.github.com>
2019-03-04 10:47:56 +0900

    Update Healthcheck.java
    오타수정

--- src/servlet/Healthcheck.java
+++ src/servlet/Healthcheck.java
@@ -1,21 +1,25 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
+
 import model.Health;
 import model.HealthCheckLogic;
 
-@WebServlet("/Healthcheck")
-public class Healthcheck extends HttpServlet {
+@WebServlet("/HealthCheck")
+public class HealthCheck extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
+		//		forword
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/healthCheck.jsp");
 		dispatcher.forward(request, response);
 
@@ -23,16 +27,20 @@ public class Healthcheck extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		//		requestparameter
 		String weight = request.getParameter("weight");
 		String height = request.getParameter("height");
+		//입력치를 proverty에 설정
 		Health health = new Health();
 		health.setHeight(Double.parseDouble(height));
 		health.setWeight(Double.parseDouble(weight));
+		//건강진단을 실행한 결과를 설정
 		HealthCheckLogic healthCheckLogic = new HealthCheckLogic();
 		healthCheckLogic.execute(health);
+		//리퀘스트 스코프를 저장
 
 		request.setAttribute("health", health);
-
+		//포워드
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/healthCheckResult.jsp");
 		dispatcher.forward(request, response);
 
